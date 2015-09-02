326c479148873d2c76e081d53499ac425711717e
ryo <ryo.kageyama@gmail.com>
2015-09-02 00:49:32 +0000

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "User pages" do
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
-    
+
     it { should have_content(user.name) }
     it { should have_title(user.name) }
 end
@@ -18,19 +18,31 @@ end
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
-  
+
   describe "signup" do
-    
+
     before { visit signup_path }
-    
+
     let (:submit) { "Create my account" }
-    
+
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User,:count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content("Password can't be blank") }
+        it { should have_content("Password is too short (minimum is 6 characters)") }
+        it { should have_content("Name can't be blank") }
+        it { should have_content("Email can't be blank") }
+        it { should have_content("Email is invalid") }
+      end
     end
-    
+
     describe "with valid information" do
       before do
         fill_in "Name",         with:"Example User"
@@ -41,6 +53,14 @@ end
 
       it "should create a user" do
         expect { click_button submit }.to change(User,:count).by(1)
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+        
+        it { should have_title(user.name)}
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
